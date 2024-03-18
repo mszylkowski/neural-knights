85e049e37edb8989b32143bd8d97e02116d3300d
Matias Szylkowski <mszylkowski@hotmail.com>
2024-03-18 00:06:29 -0400

    play online and proper pgn pipeline

--- pgn_to_positions.py
+++ pgn_to_positions.py
@@ -1,101 +1,61 @@
-from dataclasses import dataclass
+import argparse
+from io import BufferedIOBase
 from os import fstat
-from typing import BinaryIO, TextIO
-from chess import BLACK, Board
+from time import sleep
+from typing import BinaryIO
 from multiprocessing import Pool
 import signal
 
 import pyzstd
 import re
 
+from utils.pgn import str_to_game
 from utils.progressbar import ProgressBar, format_number
-from utils.moves import mirror_move
-
-LOWEST_ELO = 1400
-HIGHEST_ELO = 1600
 
 
-@dataclass
-class Game:
-    moves: list[tuple[str, str]]
-    """Stores the board fen and the move in UCI notation"""
-
-    control: str
-    """Time control. Eg: '300+3'"""
-
-    elo: int
-    """Average ELO rating of the game"""
-
-
-REMOVE_BRACKETS = re.compile(r"\s(?:\{[^}]+\})?\s?(?:\d+\.+\s)?")
 SCORE_SPLITTER = re.compile(r"(?:(?:1|0|1\/2)-(?:1|0|1\/2)|\*)\n\n")
-NOTATION_REMOVE = re.compile(r"[?!]")
-
-
-def str_to_game(game: str) -> Game | None:
-    moves: list[tuple[str, str]] = []
-    board = Board()
-    elo = 0
-    control = ""
-    for line in game.splitlines():
-        if line.startswith("1."):
-            if not HIGHEST_ELO > elo > LOWEST_ELO:
-                return None
-            line = NOTATION_REMOVE.sub("", line)
-            moves_str = REMOVE_BRACKETS.split(line[3:])[:-1]
-            if len(moves_str) < 10:
-                return None
-            for move in moves_str:
-                rep = (board.mirror() if board.turn == BLACK else board).board_fen()
-                uci = board.push_san(move).uci()
-                if board.turn == BLACK:
-                    uci = mirror_move(uci)
-                moves.append((rep, uci))
-            del board
-        elif line.startswith("[BlackElo"):
-            elo += int(line[11:-2]) // 2
-        elif line.startswith("[WhiteElo"):
-            elo += int(line[11:-2]) // 2
-        elif line.startswith("[TimeControl"):
-            control = line[14:-2]
-            base = control.split("+")[0]
-            if base != "-" and int(base) < 150:
-                return None
-    if not len(moves):
-        # Abandoned games without any moves.
-        return None
-    return Game(moves, control, elo)
+DATE_FROM_ZST = re.compile(r"\d{4}-\d{2}")
 
 
 def init_worker():
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
 
+def strs_to_games(game_strs: list[str]):
+    return [str_to_game(game) for game in game_strs]
+
+
 class ChessReader(BinaryIO):
-    def __init__(self, storage: TextIO) -> None:
+    def __init__(self, storage: BufferedIOBase) -> None:
         super().__init__()
 
         self.training_size = 0
+        self.to_process_games = 0
+        self.processed_games = 0
         self.games = 0
         self.last_part = ""
-        self.pool = Pool(1, initializer=init_worker)
+        self.pool = Pool(None, initializer=init_worker)
         self.storage = storage
 
     def write(self, n: memoryview) -> int:
         games_str = self.last_part + n.tobytes().decode(encoding="ISO-8859-1")
         games = SCORE_SPLITTER.split(games_str)
-        self.last_part = games[-1]
-        self.last_res = self.pool.map_async(
-            str_to_game, games[:-1], callback=self.results
+        self.last_part = games.pop()
+        self.to_process_games += len(games)
+        self.last_res = self.pool.apply_async(
+            strs_to_games, [games], callback=self.results
         )
         return len(n)
 
     def results(self, games):
         for game in games:
             if game is not None:
-                self.storage.writelines([",".join(x) + "\n" for x in game.moves])
+                self.storage.writelines(
+                    [(",".join(x) + "\n").encode("utf-8") for x in game.moves]
+                )
                 self.training_size += len(game.moves)
                 self.games += 1
+        self.processed_games += len(games)
 
     def size(self) -> int:
         return self.games
@@ -106,43 +66,57 @@ class ChessReader(BinaryIO):
         self.pool.join()
 
 
-def read_pgns_from_zstd(input_stream: BinaryIO, output_stream: TextIO):
-    """
-    Reads and returns the games from a zstd compressed file.
-    """
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="PGN to FEN", description="Converts PGN files to FEN format."
+    )
+    parser.add_argument(
+        "--input",
+        "-i",
+        type=argparse.FileType("rb"),
+        help="Path to the PGN file. Should be `data/*.pgn.zst`",
+        required=True,
+    )
+    parser.add_argument("--elo", "-e", type=int, default=1500)
+    args = parser.parse_args()
+
+    date = DATE_FROM_ZST.findall(args.input.name)[0]
+
+    # Read the dataset file for February 2018 (with 17M games), and pass it to the reader decompressed.
+    # Download it from https://database.lichess.org/#standard_games
+    output_stream = pyzstd.ZstdFile(f"data/fen_{args.elo}_{date}.fen.zst", "w")
+
     processor = ChessReader(output_stream)
 
     bar = ProgressBar(
-        total=fstat(input_stream.fileno()).st_size,
+        total=fstat(args.input.fileno()).st_size,
         desc="Reading PGNs",
         unit="b",
     )
 
-    def cb(total_input, total_output, read_data, write_data):
+    def cb(total_input, _to, _rd, _wd):
         bar.set(total_input)
         bar.set_postfix_str(
-            f"{format_number(processor.size())} games, {format_number(processor.training_size)} pos"
+            f"{format_number(processor.size())}/{format_number(processor.processed_games)} games, {format_number(processor.training_size)} pos"
         )
 
     pyzstd.decompress_stream(
-        input_stream,
+        args.input,
         processor,
         callback=cb,
     )
+
+    bar = ProgressBar(total=processor.to_process_games, unit="games", colour="yellow")
+    while processor.to_process_games > processor.processed_games:
+        sleep(0.01)
+        bar.set(processor.processed_games)
+        bar.set_postfix_str(
+            f"{format_number(processor.size())}/{format_number(processor.processed_games)} games, {format_number(processor.training_size)} pos"
+        )
 
     print("Cleaning")
 
     processor.close()
     bar.close()
 
-
-if __name__ == "__main__":
-    # Read the dataset file for February 2018 (with 17M games), and pass it to the reader decompressed.
-    # Download it from https://database.lichess.org/#standard_games
-    input_stream = open("data/lichess_db_standard_rated_2018-02.pgn.zst", "rb")
-    output_stream = open("data/fen_1500_2018-02.fen.txt", "w")
-
-    read_pgns_from_zstd(input_stream, output_stream)
-
-    input_stream.close()
     output_stream.close()
--- utils/ProgressBar.py
+++ utils/ProgressBar.py
@@ -3,7 +3,10 @@ from tqdm import tqdm
 
 class ProgressBar(tqdm):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **{"colour": "green", "unit_scale": True, **kwargs})
+        super().__init__(
+            *args,
+            **{"colour": "green", "unit_scale": True, "dynamic_ncols": True, **kwargs},
+        )
 
     def set(self, val):
         super().update(val - self.n)
