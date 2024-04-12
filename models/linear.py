7ac0438e2df3836954884aa45bc54eb5ad5f9db5
Reuven Brener <rabrener@gmail.com>
2024-04-12 11:36:19 -0400

    Add linear model with config.

---
+++ models/linear.py
@@ -0,0 +1,31 @@
+from torch import nn
+import torch
+
+from utils.moves import get_all_moves, NUM_OF_SQUARES, NUM_OF_PIECE_TYPES
+
+
+class Linear(nn.Module):
+    def __init__(self, device: torch.device | None = None, hid_scaler: int = 5) -> None:
+        super().__init__()
+        self.inputs = NUM_OF_SQUARES * NUM_OF_PIECE_TYPES
+        self.outputs = len(get_all_moves())
+        hidden_size = self.inputs * hid_scaler
+
+        self.fc1 = nn.Linear(self.inputs, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, self.outputs)
+        self.activation = nn.ELU()
+
+        if device:
+            self.to(device)
+
+    def forward(self, x: torch.Tensor):
+        x = x.view(x.size(0), -1)
+        x = x.type(torch.float32)
+        x = self.fc1(x)
+        x = self.activation(x)
+        x = self.fc2(x)
+        x = self.activation(x)
+        x = self.fc3(x)
+        return x
+
